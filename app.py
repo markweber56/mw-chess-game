a5fb76c4f0321824948e78ac5ae69f656481e810
mark weber <markweber56@gmail.com>
2022-01-15 16:38:59 -0600

    test loading env vars, import socketio

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
+import os
+
 from flask import Flask
 from flask_cors import CORS
+from flask_socketio import SocketIO, send
 
 app = Flask(__name__)
 cors = CORS(app, resources={r"/data": {"origins": "*"}})
+socketio = SocketIO(app, cors_allowed_origins="*")
+env = os.environ.get('env')
 
 @app.route("/")
 def index():
-        return ("hello world!")
+    return ("hello world!")
 
 @app.route("/home")
 def home():
-        return("this is the home page!")
+    return("this is the home page!")
 
 @app.route("/data")
 def data():
-    data = {'a': 1, 'b': 'a string'}
+    data = {'a': 1, 'b': 'a string', 'env': env}
     return data
+
+@socketio.on("message")
+def handleMessage():
+    print(msg)
+    send(msg, broadcast=True)
+    return
